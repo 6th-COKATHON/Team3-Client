98aeb7aedbafca0e732ae8dbc2f30e1b64b26364
ion <delion.dev@gmail.com>
2025-07-26 03:52:06 +0900

    feat/#21: ExplainerPage 컴포넌트 추가 및 라우터에 경로 설정

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,6 +2,7 @@ import SelectingRoomPage from "@/pages/SelectingRoomPage";
 import { Route, Routes } from "react-router-dom";
 import NickNamePage from "@/pages/NickNamePage";
 import GuesserPage from "@/pages/GuesserPage";
+import ExplainerPage from "@/pages/ExplainerPage";
 
 const AppRouter = () => {
   return (
@@ -10,6 +11,7 @@ const AppRouter = () => {
         <Route path="/" element={<SelectingRoomPage />} />
         <Route path="/nickname" element={<NickNamePage />} />
         <Route path="/guesser" element={<GuesserPage />} />
+        <Route path="/explainer" element={<ExplainerPage />} />
       </Routes>
     </>
   );
