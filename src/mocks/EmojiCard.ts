43ff7b0818b4e50482f624e01777658f7ba0247e
ion <delion.dev@gmail.com>
2025-07-26 02:30:16 +0900

    feat/#14: EmojiCardsMockData 추가

---
+++ src/mocks/EmojiCard.ts
@@ -0,0 +1,57 @@
+export const EmojiCardsMockData = [
+  {
+    id: 0,
+    title: "방이름방이름방이름...",
+    participants: 10,
+    isReady: false,
+    emoji: "❓",
+    gradient: {
+      from: "#2DDDAE",
+      to: "#2BC5CA",
+    },
+  },
+  {
+    id: 1,
+    title: "방이름방이름방이름...",
+    participants: 10,
+    isReady: false,
+    emoji: "❓",
+    gradient: {
+      from: "#D2AEFF",
+      to: "#8B2FFF",
+    },
+  },
+  {
+    id: 2,
+    title: "방이름방이름방이름...",
+    participants: 10,
+    isReady: false,
+    emoji: "❓",
+    gradient: {
+      from: "#D2AEFF",
+      to: "#FF7896",
+    },
+  },
+  {
+    id: 3,
+    title: "방이름방이름방이름...",
+    participants: 10,
+    isReady: false,
+    emoji: "❓",
+    gradient: {
+      from: "#FFCBD6",
+      to: "#A057FF",
+    },
+  },
+  {
+    id: 4,
+    title: "방이름방이름방이름...",
+    participants: 10,
+    isReady: false,
+    emoji: "❓",
+    gradient: {
+      from: "#A3EDFF",
+      to: "#4C7EDE",
+    },
+  },
+];
